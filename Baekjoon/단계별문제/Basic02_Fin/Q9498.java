a1480115f78c2b966f6b453dc12008d414d053c0
Kang <https://github.com/kingdoo9/Android.git>
2020-08-29 16:55:20 +0900

    2단계 완료

--- Baekjoon/단계별문제/Basic02_Fin/Q9498.java
+++ Baekjoon/단계별문제/Basic02_Fin/Q9498.java
@@ -1,4 +1,4 @@
-package Basic02; // 9498번
+package Basic02_Fin; // 9498번
 //시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
 import java.util.Scanner;
 
