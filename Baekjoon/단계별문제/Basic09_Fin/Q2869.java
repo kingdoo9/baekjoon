73da09b32010281efb4f9ed58e8258731e88aae8
60142234_강승덕 <skung723@gmail.com>
2020-08-31 18:55:53 +0900

    9단계 완료

---
+++ Baekjoon/단계별문제/Basic09_Fin/Q2869.java
@@ -0,0 +1,31 @@
+package Basic09_Fin; //Q2869
+
+/*문제
+땅 위에 달팽이가 있다. 이 달팽이는 높이가 V미터인 나무 막대를 올라갈 것이다.
+
+달팽이는 낮에 A미터 올라갈 수 있다. 하지만, 밤에 잠을 자는 동안 B미터 미끄러진다. 또, 정상에 올라간 후에는 미끄러지지 않는다.
+
+달팽이가 나무 막대를 모두 올라가려면, 며칠이 걸리는지 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 세 정수 A, B, V가 공백으로 구분되어서 주어진다. (1 <= B < A <= V <= 1,000,000,000)
+
+출력
+첫째 줄에 달팽이가 나무 막대를 모두 올라가는데 며칠이 걸리는지 출력한다.*/
+
+import java.util.Scanner;
+
+public class Q2869 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		int V = scan.nextInt();
+		int num=(V-B)/(A-B);
+		if((V-B)%(A-B) != 0) num++;
+		
+		System.out.println(num);
+		scan.close();
+	}
+}
